2e7325c863eafb181df199d1094daedbb478d4a8
Elias-Jadefrid <elias.jadefrid@elev.ga.ntig.se>
2024-11-04 14:38:59 +0100

    tester

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 class App < Sinatra::Base
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/fruits.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
 
     get '/' do
         erb(:"index")
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,31 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    db.execute('DROP TABLE IF EXISTS spel')
   end
 
+  def self.create_tables
+    db.execute('CREATE TABLE spel (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                pris INTEGER,
+                description TEXT,
+                ')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO spelat (name, pris, description) VALUES ("apex",   6, "Ett spel.")')
+    db.execute('INSERT INTO spelat (name, pris, description) VALUES ("dark souls", 8, "aaaaaaaahhhaha")')
+    db.execute('INSERT INTO vill_spela (name, pris, description) VALUES ("blood born",  8, "snart")')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/spel.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
 
 Seeder.seed!
