86b67449d5e29ce20aa5e1a5e6fa49f4a4bd0d9e
Elias-Jadefrid <elias.jadefrid@elev.ga.ntig.se>
2024-11-11 14:48:01 +0100

    tester

--- app.rb
+++ app.rb
@@ -17,4 +17,21 @@ class App < Sinatra::Base
         @spel = db.execute('SELECT * FROM spel')
         erb(:"spel/index")
     end
+
+    get '/spel/:id' do | id |
+        result = db.execute('SELECT beskrivning FROM spel WHERE id = ?', [id]).first
+        @spel_info = result["beskrivning"] if result 
+        erb(:"spel/show")
+    end
+
+    post '/spel' do 
+        spel_namn = params[:spel_namn]
+        spel_pris =  params[:spel_pris]
+        spel_beskrivning =  params[:spel_beskrivning]
+        db.execute('INSERT INTO spel (name, pris, beskrivning) VALUES (?, ?, ?)', [spel_namn, spel_pris, spel_beskrivning])
+        redirect('/spel') 
+    end
+
+
+
 end
