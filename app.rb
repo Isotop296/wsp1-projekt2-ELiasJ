667199757b5a5a8906e490d78f48f1bfbe23e575
Elias-Jadefrid <elias.jadefrid@elev.ga.ntig.se>
2024-12-11 17:57:23 +0100

    test

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 class App < Sinatra::Base
-    @user = nil
+    @user = "anonym"
 
     def db
         return @db if @db
@@ -12,40 +12,66 @@ class App < Sinatra::Base
  
 
     get '/' do
-        redirect('/spel')
-    end
-
-    get '/spel' do
-        @spel_klara = db.execute('SELECT * FROM spel where kategori = ?', ["true"])
-        @spel_oklara = db.execute('SELECT * FROM spel where kategori = ?', ["false"])
-        erb(:"spel/index")
+        redirect('/loggin')
     end
 
     get '/loggin' do
         erb(:"spel/loggin")
     end
 
-    post "/loggin/new" do
+    post "/loggin/check" do
         username = params[:username]
         password = params[:password]
     
         user_exists = db.execute('SELECT * FROM users WHERE username = ?', [username]).first
+        @admin_status = db.execute('SELECT admin FROM users WHERE username = ?', [username]).first
     
+        if user_exists
+            @user = username
+            if @admin_status["admin"] == "admin"
+                redirect('/admin')
+            else
+                redirect('/spel') 
+            end
+        else 
+            @error_message = "user dosent exist or you have spelt somthing wrong"
+            return erb(:"spel/loggin")
+        end
+    end
+
+
+    post "/loggin/new" do 
+        username = params[:username]
+        password = params[:password]
+
+        user_exists = db.execute('SELECT * FROM users WHERE username = ?', [username]).first
+
         if user_exists
             @error_message = "username already in use or inapropriet, choose another one"
             return erb(:"spel/loggin")
-        else
-            password_hash = BCrypt::Password.create(password)
-            db.execute('INSERT INTO users (username, password, admin) VALUES (?, ?, ?)', [username, password_hash, admin])
-            redirect("/")
+        else 
+        password_hash = BCrypt::Password.create(password)
+        db.execute('INSERT INTO users (username, password, admin) VALUES (?, ?, false)', [username, password_hash])
+        @user = username
+        redirect('/spel')
         end
     end
-    
-    get "/loggin/error" do 
 
+
+    get '/admin' do 
+        erb(:"spel/admin_site")
+    end
+
+    post '/admin/redirect' do
+        redirect('/spel')
     end
 
 
+    get '/spel' do
+        @spel_klara = db.execute('SELECT * FROM spel where kategori = ?', ["true"])
+        @spel_oklara = db.execute('SELECT * FROM spel where kategori = ?', ["false"])
+        erb(:"spel/index")
+    end
 
     get '/spel/:id' do | id |
         result = db.execute('SELECT beskrivning FROM spel WHERE id = ?', [id]).first
@@ -58,7 +84,8 @@ class App < Sinatra::Base
         spel_pris =  params[:spel_pris]
         spel_beskrivning =  params[:spel_beskrivning]
         spel_kategori = params[:spel_kategori]
-        db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori) VALUES (?, ?, ?, ?)', [spel_namn, spel_pris, spel_beskrivning, spel_kategori])
+        user = @user
+        db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori, kreator) VALUES (?, ?, ?, ?, ?)', [spel_namn, spel_pris, spel_beskrivning, spel_kategori, user])
         redirect('/spel') 
     end
 
@@ -78,9 +105,8 @@ class App < Sinatra::Base
         namn = params[:spel_namn]
         pris = params[:spel_pris]
         beskrivning = params[:spel_beskrivning]
-        kreator = params[:spel_kreator]
-        spel_kategori = @user
-        db.execute('UPDATE spel SET namn = ?, pris = ?, beskrivning = ?, kategori = ?, kreator = ? WHERE id = ?', [namn, pris, beskrivning, spel_kategori, spel_kreator, id])
+        spel_kategori = params[:spel_kategori]
+        db.execute('UPDATE spel SET namn = ?, pris = ?, beskrivning = ?, kategori = ? WHERE id = ?', [namn, pris, beskrivning, spel_kategori, id])
         redirect "/spel"
     end
 
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,5 +1,7 @@
 require 'sqlite3'
 require 'bcrypt'
+
+
 class Seeder
 
   def self.seed!
@@ -21,24 +23,27 @@ class Seeder
                 pris INTEGER,
                 beskrivning TEXT,
                 kategori bolean,
-                kreator text)
+                kreator integer)
                 ')
     db.execute('CREATE TABLE users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL,
                 password integer,
-                admin bolean
+                admin text
                 )
                   ')
   end
 
   def self.populate_tables
-    db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori, kreator ) VALUES ("apex",   6, "Ett spel", "true", "")')
-    db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori, kreator) VALUES ("dark souls", 8, "aaaaaaaahhhaha", "false", "")')
+    db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori, kreator ) VALUES ("apex",   6, "Ett spel", "true", "Anonym")')
+    db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori, kreator) VALUES ("dark souls", 8, "aaaaaaaahhhaha", "false", "Anonym")')
     db.execute('INSERT INTO spel (namn, pris, beskrivning, kategori, kreator) VALUES ("blood born",  8, "snart", "true", "ola")')
     password_hashed = BCrypt::Password.create("123")
+    password_hashed2 = BCrypt::Password.create("296")
     db.execute('INSERT INTO users (username, password, admin) 
-			 VALUES (?, ?)', ["ola", password_hashed, false])
+			 VALUES (?, ?, ?)', ["ola", password_hashed, "user"])
+    db.execute('INSERT INTO users (username, password, admin) 
+			 VALUES (?, ?, ?)', ["Elias", password_hashed2, "admin"])
 
   end
 
